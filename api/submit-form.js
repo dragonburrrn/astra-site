b1cc9db8e736cbe17ea12edda80edb8669a27883
Петрова Анастасия <99740820+dragonburrrn@users.noreply.github.com>
2025-03-19 00:48:44 +0300

    Update submit-form.js

--- api/submit-form.js
+++ api/submit-form.js
@@ -1,30 +1,42 @@
-const { createClient } = require('@supabase/supabase-js');
+import { createClient } from '@supabase/supabase-js';
 
 // Подключение к Supabase
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
     if (req.method === 'POST') {
+        console.log('Получены данные:', req.body); // Логируем входящие данные
+
         const { first_name, last_name, email, phone, birth_date, location, services, specialist } = req.body;
 
         try {
             // Сохраняем данные пользователя
+            console.log('Вставляем данные пользователя:', { first_name, last_name, email, phone, birth_date, location });
             const { data: user, error: userError } = await supabase
                 .from('astra_users')
-                .insert([{ first_name, last_name, email, phone, birth_date, location }], { returning: 'representation' }) // Указываем, что хотим получить данные вставленной записи
+                .insert([{ first_name, last_name, email, phone, birth_date, location }], { returning: 'representation' })
                 .single();
 
-            if (userError) throw userError;
+            if (userError) {
+                console.error('Ошибка при вставке пользователя:', userError);
+                throw userError;
+            }
+
+            console.log('Пользователь успешно создан:', user);
 
             // Сохраняем записи на услуги
             for (const service_id of services) {
+                console.log('Вставляем запись на услугу:', { user_id: user.id, service_id, specialist_id: specialist });
                 const { error: appointmentError } = await supabase
                     .from('astra_appointments')
                     .insert([{ user_id: user.id, service_id, specialist_id: specialist }]);
 
-                if (appointmentError) throw appointmentError;
+                if (appointmentError) {
+                    console.error('Ошибка при вставке записи:', appointmentError);
+                    throw appointmentError;
+                }
             }
 
             // Отправляем успешный ответ
